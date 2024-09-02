da4eb3b548a9550b458a21c56d19d50620848e74
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-09-03 00:41:33 +0530

    SIH analysis going on

--- Courses/1sep/1.py
+++ Courses/1sep/1.py
@@ -1,11 +1,12 @@
 import pandas as pd
 # pd.set_option('display.max_rows', None)
-# pd.set_option('display.max_columns', None)
+pd.set_option('display.max_columns', None)
 df = pd.read_excel("Courses\\1sep\\sihx.xlsx")
 # print(df.iloc[131])
 
-print(df.loc[df["Team Id"] == 96])
-print(df["Problem Statement ID"] == 1614)
+# print(df.loc[df["Name3"] == "Eshit Saini"])
+print(df.loc[df["Team Id"] == 148])
+# print(df["Problem Statement ID"] == 1614)
 # print(df.iloc[233])
 # l = []
 
