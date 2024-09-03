2e3cdcf81fd0d661724fad2bcac100ed59eb459e
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-09-03 10:13:31 +0530

    protyoya missing in sheer

--- Courses/1sep/1.py
+++ Courses/1sep/1.py
@@ -1,13 +1,14 @@
 import pandas as pd
 # pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
-df = pd.read_excel("Courses\\1sep\\sihx.xlsx")
+df = pd.read_excel("Courses\\1sep\\sihfr.xlsx")
 # print(df.iloc[131])
-
+# print(df)
 # print(df.loc[df["Name3"] == "Eshit Saini"])
-print(df.loc[df["Team Name"] == "Chilly Flicks" ])
+# print(df.loc[df["Team Id"] == "405a"])
 # print(df["Problem Statement ID"] == 1614)
 # print(df.iloc[233])
+print(df.loc[df["Team Name"] == "Chilly Flicks"])
 # l = []
 
 # for i in range(0, 401):
