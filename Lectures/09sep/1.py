a8fb8c589163c36679422e2dc45b27467de0a023
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-09-09 12:56:37 +0530

    basic plotting notes

---
+++ Lectures/09sep/1.py
@@ -0,0 +1,22 @@
+import seaborn as sns
+import pandas as pd
+
+# Scatter plot used for correlation
+# Box plot used for outlier detection, spread and skewness of data
+#
+# Skewness of data is the inclination of data points over a time period
+# Record, Visualize and Analyze are three major aspects of Visualisation
+# 
+# Violin Plot: Distribution of data across different categories
+# 
+# HeatMap: Correlation matrix with different stages being represented by diff colors.
+# 
+# Area Plot: 
+# 
+# Bubble Chart: Visualising multi dimensional data, used to show importance between different data points
+# 
+# Joint plot: Scatter PLot + Histogram
+# 
+# 3D plot, facet grid: Jesus christ Idk
+# 
+# #
