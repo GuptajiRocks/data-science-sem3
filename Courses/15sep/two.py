4eb1a8abfe716a2617d5c75eeee68826111538bd
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-09-15 23:47:38 +0530

    tkinter app built like ChatGPT

--- Courses/15sep/two.py
+++ Courses/15sep/two.py
@@ -1,32 +1,37 @@
 import tkinter as tk
 from tkinter import ttk
 import google.generativeai as genai
+import mysql.connector as mp
 
+connection = mp.connect(host='localhost', database="pysqlone",user="root", password="root")
 genai.configure(api_key="AIzaSyDMhYAHLqk1AjmMUQ0Eby5ycLlNDM92x10")
 model = genai.GenerativeModel("gemini-1.5-flash")
+cursor = connection.cursor()
+cursor.execute("USE pysqlone;")
+
 
 def process_text():
     x = str(entry.get())
     response = model.generate_content(x)
+    otpt = response.text
     label.configure(text=response.text)
+    insert_query = f"INSERT INTO prompthistory VALUES(\"{x}\", \"{otpt}\");"
+    cursor.execute(str(insert_query))
+    connection.commit()
 
 root = tk.Tk()
 root.title("Text Processing Dialog")
 root.geometry("300x200")
 
-# Create a frame for the widgets
 frame = ttk.Frame(root, padding=(10, 10))
 frame.pack()
 
-# Create the text entry field
 entry = ttk.Entry(frame, width=30)
 entry.pack(pady=10)
 
-# Create the button
 button = ttk.Button(frame, text="Process Text", command=process_text)
 button.pack(pady=10)
 
-# Create the label for displaying output
 label = ttk.Label(frame, text="")
 label.pack(pady=10)
 
