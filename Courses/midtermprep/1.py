9d55b734667d94947a2c55e35c4ac61ae7cdbd00
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-10-09 20:10:06 +0530

    prepping 1

---
+++ Courses/midtermprep/1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = {"A":[3,2,1], "B":[4,5,6], "C":[7,8,9]}
+df = pd.DataFrame(data)
+#print(df.iloc[[1,2],[1,2]])
+
+# dictionary database when given, always convert to row column form, asaan rahega
+
+print(df.sort_values("A"))
