8aaf536e25c3270c72647777185b24c23b1e7169
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-10-30 20:34:37 +0530

    30 oct

--- Practical/21oct (28oct)/1.py
+++ Practical/21oct (28oct)/1.py
@@ -1,17 +1,12 @@
 import numpy as np
-l = [10,20,30,5,50]
+l = [10,20,30,5,50,72]
 m = np.mean(l)
 newl = []
-
-std_dev = 0
-
-for i in l:
-    std_dev = std_dev + (i-m)
-
-tv = std_dev/len(l)
-res = tv**0.5
+std = 0
 
 for i in l:
-    newl.append((i-m)/res)
+    std = std + (i-m)
+    print(std)
 
-print(newl)
+print(std)
+    
