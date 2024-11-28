61a753fc831c2712b7f8b8ae9330aca1af9a7adb
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-11-28 22:56:06 +0530

    all done working

--- Courses/gpa/app.py
+++ Courses/gpa/app.py
@@ -7,15 +7,46 @@ app = Flask(__name__)
 def main_page():
     return render_template("index.html")
 
-@app.route('/marks', methods=['POST'])
+@app.route('/marks', methods=['POST', 'GET'])
 def marks():
-    selected_subjects = request.form.getlist('subjects')  # List of selected subjects
+    selected_subjects = request.form.getlist('subjects') 
     subject_values = {}
+    cgl = []
     for subject in selected_subjects:
-        value = request.form.get(f"{subject}_value")
-        subject_values[subject] = value
-
-    return jsonify({"selected_subjects_with_values": subject_values})
+        if subject == "cpp":
+            value = request.form.get(f"{subject}_value")
+            cgl.append(value)
+            subject_values["Data Structures using C++"] = value
+        elif subject == "ims":
+            value = request.form.get(f"{subject}_value")
+            cgl.append(value)
+            subject_values["Information Management Systems"] = value
+        elif subject == "spec":
+            value = request.form.get(f"{subject}_value")
+            cgl.append(value)
+            subject_values["Specialization"] = value
+        elif subject == "prob":
+            value = request.form.get(f"{subject}_value")
+            cgl.append(value)
+            subject_values["Probability and Statistics"] = value
+        elif subject == "swe":
+            value = request.form.get(f"{subject}_value")
+            cgl.append(value)
+            subject_values["Software Engineering"] = value
+        else:
+            pass
+    
+    credit_sum = 23
+    based = 0
+    for i in range(len(cgl)):
+        if i == 0:
+            based = based + int(cgl[i])*7
+        else:
+            based = based + int(cgl[i])*4
+    
+    finalcg = based/credit_sum
+    
+    return render_template('output.html', data=subject_values, specific_data=finalcg)
 
 if __name__ == "__main__":
     app.run(debug=True)
